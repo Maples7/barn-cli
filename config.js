e9c2f9e570d8fc90ce18929d81651474b6529808
Maples7 <maples7@163.com>
2017-03-16 01:56:06 +0800

    finish html template rendering

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // barn init
+  barnStarterURL: 'git@github.com:Maples7/barn.git',
+
+  // barn server
+  debug: {
+    host: 'localhost',
+    port: 7000
+  },
+
+  // barn generate
+  configFileName: 'config.yml',
+  contentFileName: 'content.yml'
+
+  // barn deploy
+};
